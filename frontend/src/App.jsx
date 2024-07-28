276b2816167fd1f087b4d1f558ad7adb4103693e
nam <namhoangpham96@gmail.com>
2024-07-27 20:21:57 -0400

    Minor edit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
-import react from "react"
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 
 
