788007a491b06daef4daef0f9a6eb8e356f1c74c
nam <namhoangpham96@gmail.com>
2024-07-28 23:17:53 -0400

    Make the current URL for the front end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,37 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { Home } from './pages';
+import { Home, Login, Register, NotFound } from './pages'
+import ProtectedRoute from "./components/ProtectedRoute"
 
+function Logout() {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
 
 
-        {/* <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+
+        
+        <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<Logout />} />
         <Route path="/register" element={<RegisterAndLogout />} />
-        <Route path="*" element={<NotFound />}></Route> */}
+        <Route path="*" element={<NotFound />}/>
       </Routes>
     </BrowserRouter>
   )
