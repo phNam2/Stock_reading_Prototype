0d579ec7a4e2fd6bbfcfad90f129f1ca3a88de51
nam <namhoangpham96@gmail.com>
2024-07-28 23:15:28 -0400

    Update the Not Found page

--- frontend/src/pages/NotFound/NotFound.jsx
+++ frontend/src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,8 @@
 function NotFound() {
-    return (
-        <h1>NotFound is here.</h1>
-    )
+    return <div>
+                <h1>404 Not Found</h1>
+                <p>The page you are looking for does not exist!</p>
+           </div>
 }
 
 export default NotFound
