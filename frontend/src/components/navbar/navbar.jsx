4947b28a450bb61c8d8d355760b969b29d250100
nam <namhoangpham96@gmail.com>
2024-07-29 22:21:49 -0400

    Minor change for the navbar design

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -12,45 +12,60 @@ function NavBar() {
 
     useEffect(() => {
         console.log(location)
-        if (location.pathname === '/login' || location.pathname === '/register') {
-            setIsLoggedIn(false)
+        if (location.pathname === '/login') {
+            setIsLoggedIn("login")
+        } else if (location.pathname === '/register'){
+            setIsLoggedIn("register")
         } else {
-            setIsLoggedIn(true)
+            setIsLoggedIn("else")
             setCurrentName(localStorage.getItem(currentUser))
         }
     }, [location])
 
     return (
         <div>
-            { isLoggedIn ? (
-                <nav className="navbar">
-                    <ul className="nav-list">
-                        <li className="nav-item">
-                            <h1>NavBar is here for {currname}</h1>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/logout" className="nav-link">
-                                Logout
-                            </Link>
-                        </li>
-                    </ul>
-                </nav>
-            ): (
-                <nav className="navbar">
-                    <ul className="nav-list">
-                        <li className="nav-item">
-                            <Link to="/login" className="nav-link">
-                                Login
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/register" className="nav-link">
-                                Register
-                            </Link>
-                        </li>
-                    </ul>
-                </nav>
-            )}  
+            {(() => {
+                if (isLoggedIn === "else") {
+                  return (
+                    <nav className="navbar">
+                        <ul className="nav-list">
+                            <li className="nav-item">
+                                <h1>NavBar is here for {currname}</h1>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/logout" className="nav-link">
+                                    Logout
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                  )
+                } else if (isLoggedIn === "login") {
+                  return (
+                    <nav className="navbar">
+                        <ul className="nav-list">
+                            <li className="nav-item">
+                                <Link to="/register" className="nav-link">
+                                    Register
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                  )
+                } else {
+                  return (
+                    <nav className="navbar">
+                        <ul className="nav-list">
+                            <li className="nav-item">
+                                <Link to="/login" className="nav-link">
+                                    Login
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                  )
+                }
+              })()}  
         </div>
         
         
