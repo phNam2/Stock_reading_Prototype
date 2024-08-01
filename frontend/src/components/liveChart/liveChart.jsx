298c612f822d26a1d104839719ebb0f038fac4ab
nam <namhoangpham96@gmail.com>
2024-08-01 01:00:09 -0400

    Fix the multiple API call problem

--- frontend/src/components/liveChart/liveChart.jsx
+++ frontend/src/components/liveChart/liveChart.jsx
@@ -1,19 +1,24 @@
-import React, { useEffect, useMemo, useState } from 'react'
-import { fetchStockDataPolygon } from '../../services'
+import React, { useEffect, useState, useRef } from 'react'
+import { fetchStockDataPolygon, fetchStockDataAlphaVantage } from '../../services'
 
 
 function liveChart({symbol}) {
 
     const [stockData, setStockData] = useState({})
+    var effectRan = useRef(false) // prevent the API calling many times
 
     useEffect(() => {
-        fetchStockDataPolygon(symbol).then(data =>
-            setStockData(data)
-        )
+        if (effectRan.current===false){
+            fetchStockDataPolygon(symbol).then(data =>
+                setStockData(data)
+            )
+        }
+        return () => {
+            console.log("unmounted")
+            effectRan = true
+        }
     }, [])
-
     console.log(stockData)
-    
 
     return (
         <h1>Live Chart is here</h1>
--- frontend/src/services.js
+++ frontend/src/services.js
@@ -1,5 +1,6 @@
 const ALPHAVANTAGE_API_KEY = import.meta.env.VITE_ALPHAVANTAGE_API
 const POLYGON_API_KEY= import.meta.env.VITE_POLYGON_API
+import axios from "axios"
 
 export const fetchStockDataAlphaVantage = async (symbol) => {
     const response = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=${symbol}&apikey=${ALPHAVANTAGE_API_KEY}`)
@@ -9,7 +10,6 @@ export const fetchStockDataAlphaVantage = async (symbol) => {
 
 
 export const fetchStockDataPolygon = async (symbol) => {
-    const response = await fetch(`https://api.polygon.io/v1/open-close/${symbol}/2023-01-09?adjusted=true&apiKey=${POLYGON_API_KEY}`)
-    const data = await response.json()
-    return data
+    const response = await axios.get(`https://api.polygon.io/v1/open-close/${symbol}/2023-01-09?adjusted=true&apiKey=${POLYGON_API_KEY}`)
+    return response.data
 }
