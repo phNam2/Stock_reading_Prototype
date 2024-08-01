b4bfc1c880245b97bf1b11034f3c0fb26805896a
nam <namhoangpham96@gmail.com>
2024-08-01 15:25:29 -0400

    Update the code

--- frontend/src/components/liveChart/liveChart.jsx
+++ frontend/src/components/liveChart/liveChart.jsx
@@ -1,15 +1,22 @@
 import React, { useEffect, useState, useRef } from 'react'
-import { fetchStockDataPolygon, fetchStockDataAlphaVantage } from '../../services'
+import { fetchStockDataPolygon, 
+         fetchStockDataAlphaVantage, 
+         fetchAllTickerPolygon,
+         fetchAllExchangePolygon } 
+    from '../../services'
 
 
 function liveChart({symbol}) {
 
-    const [stockData, setStockData] = useState({})
+    const [stockData, setStockData] = useState(null)
     var effectRan = useRef(false) // prevent the API calling many times
 
     useEffect(() => {
         if (effectRan.current===false){
-            fetchStockDataPolygon(symbol).then(data =>
+            // fetchStockDataPolygon(symbol).then(data =>
+            //     setStockData(data)
+            // )
+            fetchAllExchangePolygon().then(data =>
                 setStockData(data)
             )
         }
@@ -19,6 +26,7 @@ function liveChart({symbol}) {
         }
     }, [])
     console.log(stockData)
+
 
     return (
         <h1>Live Chart is here</h1>
