f0cc2f04e42c86ee5573c585cdc0229289947177
nam <namhoangpham96@gmail.com>
2024-08-06 16:55:50 -0400

    Current CandleStick chart design

--- frontend/src/components/liveChart/CandleStick/CandleStick.jsx
+++ frontend/src/components/liveChart/CandleStick/CandleStick.jsx
@@ -9,14 +9,18 @@ function CandelStickChart() {
           id: "candlestick-chart",
         },
         xaxis: {
-          categories: ["Jan", "Feb", "Mar", "Apr", "May"], 
+          categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"], 
+          type: 'datetime',
         },
       }
     
     const series = [
         {
           data: [
-            [1546300800000, 100, 120, 80, 110],
+            ['2012-08-08', 100, 120, 80, 110],
+            ['2012-08-09', 100, 120, 80, 110],
+            ['2012-08-10', 100, 120, 80, 110],
+            ['2013-10-10', 100, 120, 80, 110],
           ],
         },
     ]
