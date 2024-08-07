1424408fc39e98bb618b9eec84cde41b31c625d0
nam <namhoangpham96@gmail.com>
2024-08-07 16:55:20 -0400

    Add the function to draw the candle stick chart

--- frontend/src/components/liveChart/liveChart.jsx
+++ frontend/src/components/liveChart/liveChart.jsx
@@ -5,39 +5,41 @@ import { fetchStockDataAlphaVantage,
     from '../../services'
 
 
-function liveChart({symbol}) {
+function liveChart() {
 
     const [stockDataMeta, setStockDataMeta] = useState([])
     const [stockDataValues, setStockDataValues] = useState([])
-    var effectRan = useRef(false) // prevent the API calling many times
 
+    
+    var effectRan = useRef(false) // prevent the API calling many times
     useEffect(() => {
         if (effectRan.current===false){
-            fetchStockTwelveDataTimeSeries(symbol).then(data => {
-                setStockDataMeta(data[0])
-                setStockDataValues(data[1])
-            })
+            
         }
         return () => {
             console.log("unmounted")
             effectRan = true
         }
     }, [])
-    console.log(stockDataMeta)
 
+
+    // Function used to draw the CandleStick Chart from the input symbol
+    function printCandleStickChart (symbol) {
+        fetchStockTwelveDataTimeSeries(symbol).then(data => {
+            setStockDataMeta(data[0])
+            setStockDataValues(data[1])
+        })
+    }
 
     return (
 
         <div>
             <h1>Chart Page is here for {stockDataMeta.symbol}</h1>
-
             {/* <ul>
                 {stockDataValues.map((item, index) => (
                     <h1>{item.open}</h1>
                 ))}
             </ul> */}
-
-
             <CandelStickChart stockValues={stockDataValues}/>
         </div>
     )
