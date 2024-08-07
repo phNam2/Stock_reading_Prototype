f7ac894ad7643a1ad15ca6d84465cf022e31f0bb
nam <namhoangpham96@gmail.com>
2024-08-07 12:04:30 -0400

    Remove the comment

--- frontend/src/components/liveChart/CandleStick/CandleStick.jsx
+++ frontend/src/components/liveChart/CandleStick/CandleStick.jsx
@@ -19,25 +19,16 @@ function CandelStickChart({stockValues}) {
         id: "candlestick-chart",
       },
       xaxis: {
-        // categories: [
-        //   'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-        //   'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
-        // ],
         type: 'datetime',
       },
     }
   
   const series = [
       {
-        // data: [
-        //   ['2012-08-08', 100, 120, 80, 110],
-        //   ['2012-08-09', 100, 120, 80, 110],
-        //   ['2012-08-10', 100, 120, 80, 110],
-        //   ['2013-10-10', 100, 120, 80, 110],
-        // ],
         data: parseValues
       },
   ]
+  
   
     
   return (
