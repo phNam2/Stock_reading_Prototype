eccfbb2301a521bd56c4ec6a48945610265733c5
nam <namhoangpham96@gmail.com>
2024-08-08 17:11:13 -0400

    Add the design for the result

--- frontend/src/components/searchResultList/searchResultList.jsx
+++ frontend/src/components/searchResultList/searchResultList.jsx
@@ -7,7 +7,7 @@ export const searchResultList = ({results}) => {
     return (
         <div className="results-list">
             {results.map((stock, index) => (
-                <div key={index}>{stock.symbol}</div>
+                <div key={index} className='search-result'>{stock.symbol}</div>
             ))}
         </div>
     )
